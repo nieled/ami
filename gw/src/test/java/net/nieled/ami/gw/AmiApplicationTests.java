33c60f7df449dc36eb00ae32e31bea405f5b57e4
Daniel Calle <nieled@riseup.net>
2022-08-16 19:14:11 -0500

    initial commit

---
+++ gw/src/test/java/net/nieled/ami/gw/AmiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nieled.ami.gw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ msa/src/main/java/net/nieled/ami/msa/AmiApplication.java
@@ -0,0 +1,13 @@
+package net.nieled.ami.msa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmiApplication.class, args);
+    }
+
+}
