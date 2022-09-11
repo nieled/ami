b0fd006a180844f866876d1d33c8590437e96716
Daniel Calle <nieled@riseup.net>
2022-09-11 17:11:38 -0500

    Update vite build config

--- gw/vite.config.ts
+++ gw/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    root: 'src/main/webapp',
-    plugins: [react()]
+  root: 'src/main/webapp',
+  build: {
+    outDir: '../../../build/resources/main/static/app',
+  },
+  plugins: [react()],
 })
