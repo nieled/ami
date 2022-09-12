502f4eaea6faf9a59b0a503507708e90dc2a0168
Daniel Calle <nieled@riseup.net>
2022-09-11 21:45:51 -0500

    add vite config for tests

--- gw/vite.config.ts
+++ gw/vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: 'src/main/webapp',
+  root: "src/main/webapp",
   build: {
-    outDir: '../../../build/resources/main/static',
+    outDir: "../../../build/resources/main/static"
   },
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: false,
+  }
+});
